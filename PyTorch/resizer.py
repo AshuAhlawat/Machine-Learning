8062a76676ecb07fe5f7a16503e4f64c043e3843
Ashwani Ahlawat <ashuahlawat22@gmail.com>
2021-09-18 10:21:08 +0530

    gender classification dataset

--- PyTorch/resizer.py
+++ PyTorch/resizer.py
@@ -2,24 +2,24 @@ from PIL import Image
 import os
 
 
-u_dir = "./data/hymenoptera/train/ants/"
+u_dir = "./data/Gender/val/male"
 files = os.listdir(u_dir)
 
 x = 0
 for i in files:
     x += 1 
-    i = Image.open(u_dir+i)
-    i = i.resize((300,300))
-    i.save("./data/hymenoptera2/train/ants/"+str(x)+".png")
+    i = Image.open(u_dir+"/"+i)
+    i = i.resize((64,96))
+    i.save("./data/Gender2/val/male/"+str(x)+".jpg")
+    print(x)
 
 
-
-u_dir = "./data/hymenoptera/train/bees/"
+u_dir = "./data/Gender/val/female"
 files = os.listdir(u_dir)
 
 x = 0
 for i in files:
     x += 1 
-    i = Image.open(u_dir+i)
-    i = i.resize((300,300))
-    i.save("./data/hymenoptera2/train/bees/"+str(x)+".png")
+    i = Image.open(u_dir+"/"+i)
+    i = i.resize((64,96))
+    i.save("./data/Gender2/val/female/"+str(x)+".jpg")
